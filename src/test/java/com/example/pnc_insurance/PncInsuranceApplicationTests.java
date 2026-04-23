226cba7c9013d69fe5a038c8e7d39b7ff9ae8f95
gsvalue <gurpreet.singh1@valuemomentum.com>
2026-04-23 16:54:43 +0530

    Maven Project Created

--- src/test/java/com/example/pnc_insurance/PncInsuranceApplicationTests.java
+++ src/test/java/com/example/pnc_insurance/PncInsuranceApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.pnc_insurance;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PncInsuranceApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
